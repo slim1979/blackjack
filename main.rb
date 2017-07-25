1f7d26d9cdd37e822a563c18aa39b798fb398beb
slim1979 <chinezan@yandex.ru>
2017-07-25 20:13:08 +0300

    Shuffling

--- main.rb
+++ main.rb
@@ -9,6 +9,17 @@ class Game
     @deck ||= []
   end
 
+  def shuffling
+    3.times do
+      (0..5).each do |i|
+        print "Shuffling" + "."*i, "\r"
+        sleep 0.2
+      end
+      print "               ","\r"
+    end
+    deck.shuffle!
+  end
+
   def creating_deck
     ["\u2660","\u2665","\u2663","\u2666"].each do |suit|
       [2,3,4,5,6,7,8,9,10,'V','Q','K','A'].each do |value|
