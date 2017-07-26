0a892a9d98007852d3d4b85b91f89ee994bf6c65
slim1979 <chinezan@yandex.ru>
2017-07-27 00:45:16 +0300

    refactored

--- main.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'user.rb'
-require_relative 'dealer.rb'
-require_relative 'game.rb'
-
-puts 'Enter your name:'
-@user = User.new(gets.strip.chomp)
-@dealer = Dealer.new('Tobby')
-@game = Game.new
